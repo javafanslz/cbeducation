5a24869c0ac1d2f98f62f9fc955bdf3856605de4
lizhu <t_lizhu@channelsoft.com>
2018-06-27 21:30:59 +0800

    初始化提交

---
+++ src/main/java/com/education/CbEducationApplication.java
@@ -0,0 +1,12 @@
+package com.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbEducationApplication.class, args);
+	}
+}
