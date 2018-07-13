5464049574cf586c95a336e0127c70ecf3776367
lizhu <lizhu@channelsoft.com>
2018-07-13 09:25:51 +0800

    增加vue

---
+++ src/main/java/com/education/comtroller/IndexController.java
@@ -0,0 +1,13 @@
+/**
+ * <dl>
+ * <dt> IndexController</dt>
+ * <dd>Description:</dd>
+ * <dd>Copyright: Copyright (C) 2016</dd>
+ * <dd>Company: 青牛（北京）技术有限公司</dd>
+ * <dd>CreateDate:2018/7/12</dd>
+ * </dl>
+ *
+ * @author lizhu
+ */
+public class IndexController {
+}
