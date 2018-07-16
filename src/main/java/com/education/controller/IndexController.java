0b141da5717dd2996e6ff38dce3909d8c659dbcd
lizhu <lizhu@channelsoft.com>
2018-07-16 16:22:13 +0800

    修改文件夹名称

---
+++ src/main/java/com/education/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.education.comtroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <dl>
+ * <dt> IndexController</dt>
+ * <dd>Description:</dd>
+ * <dd>Copyright: Copyright (C) 2016</dd>
+ * <dd>Company: 青牛（北京）技术有限公司</dd>
+ * <dd>CreateDate:2018/7/12</dd>
+ * </dl>
+ *
+ * @author lizhu
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @RequestMapping(value = "/gotoIndex")
+    public String gotoIndex(){
+        return "index";
+    }
+}
--- src/main/java/com/education/intercept/WebLogAspect.java
+++ src/main/java/com/education/intercept/WebLogAspect.java
@@ -1,13 +1,13 @@
 /**
  * <dl>
- * <dt> IndexController</dt>
+ * <dt> WebLogAspect</dt>
  * <dd>Description:</dd>
  * <dd>Copyright: Copyright (C) 2016</dd>
  * <dd>Company: 青牛（北京）技术有限公司</dd>
- * <dd>CreateDate:2018/7/12</dd>
+ * <dd>CreateDate:2018/7/16</dd>
  * </dl>
  *
  * @author lizhu
  */
-public class IndexController {
+public class WebLogAspect {
 }
